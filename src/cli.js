a3e0d11e6bd54d5fa91721713eea7a547ccc9dff
dASSharya <dasharya299@me.com>
2024-02-07 17:07:15 +0300

    step third -done

---
+++ src/cli.js
@@ -0,0 +1,14 @@
+import readlineSync from 'readline-sync'; 
+const greeting = 'Welcome to the Brain Games!';
+
+
+const salution = () => {
+console.log(greeting);
+
+const nameQuestion = readlineSync.question('May I have your name? ');
+const userName = nameQuestion;
+
+console.log('Hello, ' + userName + '!');
+};
+
+export default salution;
