e6fb1555d2f01c6e3ee4482e53a6e20bd67109a4
dASSharya <dasharya299@me.com>
2024-02-08 14:00:02 +0300

    fourth step -done

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import salution from '../src/cli.js'; 
+import salution from '../src/cli.js';
 
-salution(); 
-
-
+salution();
--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,14 @@
-import readlineSync from 'readline-sync'; 
+import readlineSync from 'readline-sync';
+
 const greeting = 'Welcome to the Brain Games!';
 
-
 const salution = () => {
-console.log(greeting);
+  console.log(greeting);
 
-const nameQuestion = readlineSync.question('May I have your name? ');
-const userName = nameQuestion;
+  const nameQuestion = readlineSync.question('May I have your name? ');
+  const userName = nameQuestion;
 
-console.log('Hello, ' + userName + '!');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default salution;
